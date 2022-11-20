302e3c0a813e0cedabc9bcfab789c8e786de90d5
marcuszcoding <marcuszcoding@gmail.com>
2022-11-19 18:08:49 -0700

    refactored and simplified eqArrays function, decided to get rid of the .length condition to simplify and also the variable to return truthy or falsy

--- eqArrays.js
+++ eqArrays.js
@@ -7,22 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let result = false;
-  if (array1.length !== array2.length) {
-    return result;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        result = false;
-      } else {
-        result = true;
-      }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-  } return result;
+  }
+  return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [1, 2, 3, 4]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

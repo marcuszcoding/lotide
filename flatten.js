b31fe0eaab503286a16b5b37e94b0bb1dcc987f0
marcuszcoding <marcuszcoding@gmail.com>
2022-11-19 21:27:00 -0700

    flatten function not implemented into assert function

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqualArray = function(arr1, arr2) {  
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ The arrays are equal! ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ The arrays are not equal! ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+      result.push(array[i][j]);
+    } 
+    } else {
+       result.push(array[i]);
+    }
+  } 
+  console.log(result);
+}
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]

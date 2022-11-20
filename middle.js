ec0dbf4bbe66b09c5e9cbfca0c72dc0860d15ad7
marcuszcoding <marcuszcoding@gmail.com>
2022-11-19 21:27:42 -0700

    middle function not implemented into assert function

---
+++ middle.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqualArray = function(arr1, arr2) {  
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ The arrays are equal! ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ The arrays are not equal! ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(array) {
+  let finalArray = [];
+  if (array.length <= 2) {
+    return result;
+  } else if (array.length % 2 === 0) {
+    finalArray.push(array[array.length / 2 - 1]);
+    finalArray.push(array[array.length / 2]);
+    return finalArray;
+  } else {
+    finalArray.push(array[array.length / 2 - 0.5])
+  }
+  return finalArray;
+};
+
+//odd
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+//even
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+//Arrays with 1-2 elements = []
+// Arrays with odd elements should return a single element 
+// Arrays with even elements should return 2 elements 

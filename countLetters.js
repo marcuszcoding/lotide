955cffcb902f086596c2923822f01d01997d1b07
marcuszcoding <marcuszcoding@gmail.com>
2022-11-22 14:30:22 -0700

    Starting file

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters= function(string) {
+  //need empty object to return resulting letter + count
+  let results = {};
+  
+      
+  return results;
+}
+
+
+// Function should take in a string

c87a1f183ef1fec784885ea22a8695ca62251f48
marcuszcoding <marcuszcoding@gmail.com>
2022-11-22 15:13:14 -0700

    eslinted and implemented a if check for spaces, also fixed my for loop and added let to define letters

--- countLetters.js
+++ countLetters.js
@@ -7,20 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const countLetters= function(string) {
+const countLetters = function(string) {
 // Need empty object to return resulting letter + count
   let results = {};
-  for (letters of string) { //Loop needs to go through the letters of the string
-    if (results[letters]) {
-      results[letters] += 1; //For letters seen multiple times
-    } else {
-      results[letters] = 1; //For letters seen once
+  for (let letters of string) { //Loop needs to go through the letters of the string
+    if (letters !== " ") {
+      if (results[letters]) {
+        results[letters] += 1; //For letters seen multiple times
+      } else {
+        results[letters] = 1; //For letters seen once
+      }
     }
-  }    
-  console.log(results);
+  }
+  return results;
 };
 // Function should take in a string
-// Suggested to use for.. of loop 
+// Suggested to use for.. of loop
 // Shouldn't take in blank spaces
 
-countLetters("lighthouse in the house")
+countLetters("lighthouse in the house");

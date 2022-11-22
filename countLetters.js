b9738b99f84111ff23baedf0d0add0251b989590
marcuszcoding <marcuszcoding@gmail.com>
2022-11-22 15:08:00 -0700

    working function but still taking in spaces, going to implement a if check

--- countLetters.js
+++ countLetters.js
@@ -8,12 +8,19 @@ const assertEqual = function(actual, expected) {
 
 
 const countLetters= function(string) {
-  //need empty object to return resulting letter + count
+// Need empty object to return resulting letter + count
   let results = {};
-  
-      
-  return results;
-}
-
-
+  for (letters of string) { //Loop needs to go through the letters of the string
+    if (results[letters]) {
+      results[letters] += 1; //For letters seen multiple times
+    } else {
+      results[letters] = 1; //For letters seen once
+    }
+  }    
+  console.log(results);
+};
 // Function should take in a string
+// Suggested to use for.. of loop 
+// Shouldn't take in blank spaces
+
+countLetters("lighthouse in the house")

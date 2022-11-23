f880e71c175ababa307f1d45812ed7bc9b5e6425
marcuszcoding <marcuszcoding@gmail.com>
2022-11-23 09:36:09 -0700

    Added in my eqArray function for later

--- eqObjects.js
+++ eqObjects.js
@@ -5,6 +5,16 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {

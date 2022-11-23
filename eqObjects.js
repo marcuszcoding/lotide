a706987eeb7bf092debb783af913842b73967a5e
marcuszcoding <marcuszcoding@gmail.com>
2022-11-23 09:14:21 -0700

    currently working length check and setting up to check if values are equal in the objects

--- eqObjects.js
+++ eqObjects.js
@@ -8,10 +8,17 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  let lengthObj1 = Object.keys(object1).length
-  let lengthObj2 = Object.keys(object2).length
-  if (lengthObj1 === lengthObj2) {
-
+  let obj1 = Object.keys(object1)
+  let obj2 = Object.keys(object2)
+  if (obj1.length !== obj2.length) {
+    return false
+  } else {
+    for (const key in object1) {
+      console.log(object1[key]) //making sure we are returning the value of the key
+      // if (object1[key] !== object2[key]{
+      //   return false
+      // } 
+    }
   }
 };
 

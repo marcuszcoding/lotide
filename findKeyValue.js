bbecfdb3ed14ee979ba2a7a3bd9d2ffeadb163d9
marcuszcoding <marcuszcoding@gmail.com>
2022-11-22 19:28:39 -0700

    eslinted

--- findKeyValue.js
+++ findKeyValue.js
@@ -6,19 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 // Main goal: to make a function that takes in an object and a value
-// This function must look for the value in the created object and 
+// This function must look for the value in the created object and
 // return the key if it is in the object, or undefined if it is not
 // Recommended for of loop and Object.keys
 const findKeyByValue = function(object, value) {
   const objKeys = Object.keys(object); // Variable that puts the objects key names in an array
   for (const key of objKeys) {
     if (object[key] === value) { // If the value is is located object[key]
-      return key // returns paired key and if not found already returns undefined
-    } 
+      return key; // returns paired key and if not found already returns undefined
+    }
   }
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"

5ab4a3282f5beb4e34bff829c0bc2d80594b038e
marcuszcoding <marcuszcoding@gmail.com>
2022-11-23 19:51:26 -0700

    functioning assert object function, linted and tested

---
+++ assertObjectsEqual.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  // Checking if the lengths of each object are the same
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false; // returns flase if objects arent the same length
+  }
+  for (const key in object1) {
+    if (Array.isArray(object1[key])) { // if the value is an array in the object
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`❌❌❌ Assertion Passed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
+

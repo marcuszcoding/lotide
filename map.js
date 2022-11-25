a52a5bb6e00881282616e5fc2070e691cb513612
marcuszcoding <marcuszcoding@gmail.com>
2022-11-25 11:28:13 -0700

    eslinted and working map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  console.log(array2);
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Passed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+// console.log(results1);
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+

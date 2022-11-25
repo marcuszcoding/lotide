2581b5151c37263968b82d66c683a37f33801f52
marcuszcoding <marcuszcoding@gmail.com>
2022-11-25 12:10:07 -0700

    added assert tests that are passing

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Passed: ${arr1} !== ${arr2}`);
+  }
+};
+
 const takeUntil = function(array, callback) {
   let answer = [];
   for (const element of array) {
@@ -19,3 +39,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ])

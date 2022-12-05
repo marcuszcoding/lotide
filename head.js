f6333cc7992e807cedad6cd9d848a597e61c05f9
marcuszcoding <marcuszcoding@gmail.com>
2022-12-05 11:38:37 -0700

    added module export

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   if (array.length !== 0 && Array.isArray(array) === true) {
     return array[0];
   }
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),undefined);
+module.exports = head

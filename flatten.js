ff5e0b23ea64d8d1d0220940324a704d71a3f1bb
marcuszcoding <marcuszcoding@gmail.com>
2022-12-06 13:07:04 -0700

    eslint

--- flatten.js
+++ flatten.js
@@ -1,7 +1,7 @@
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false
-  };  
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -33,3 +33,5 @@ const flatten = function(array) {
 };
 
 flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
+
+module.exports = flatten;

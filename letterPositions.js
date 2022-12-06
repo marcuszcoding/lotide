1701fdae4dc900bd3ccc233f14aaf94cedc2c54e
marcuszcoding <marcuszcoding@gmail.com>
2022-12-06 12:49:32 -0700

    eslint

--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,7 @@
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false
-  };
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -36,3 +36,5 @@ const letterPositions = function(sentence) {
 
 letterPositions("lighthouse in the house");
 assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;
